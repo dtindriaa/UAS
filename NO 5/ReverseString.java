e1bd8f704528dcf085f37f772a70d3ec575942f2
Dita indriawati <80086227+dtindriaa@users.noreply.github.com>
2021-07-11 20:55:31 +0700

    Create ReverseString.java

---
+++ NO 5/ReverseString.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    
+    public static void main(String args[]) {
+        
+        String kataNormal;
+        String kataBalik = "";
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Input kata atau kalimat disini : ");
+        
+        kataNormal = input.nextLine();
+        
+        for (int i = kataNormal.length() - 1; i >= 0; i--)
+        {
+            kataBalik += kataNormal.charAt(i);
+        }
+        
+        System.out.println("Hasil kata yang di balik ; " + kataBalik );
+    
+        }
+}
